7bb464443c8e3f1c9ab846fe6221d171109df48c
Hassan Diab <hasan.diab.022001@gmail.com>
2024-01-17 15:10:14 +0200

    minor changes

--- editQuizAction.php
+++ editQuizAction.php
@@ -10,7 +10,7 @@ if (isset($_SESSION["LoggedIN"]) && $_SESSION["LoggedIN"]) {
     $quizTitle = isset($_POST['quiz_title']) ? $_POST['quiz_title'] : '';
     $description = isset($_POST['description']) ? $_POST['description'] : '';
 
-
+    
     // Validate and sanitize the data
     $quiz_id = htmlspecialchars(trim($quiz_id));
     $quizTitle = htmlspecialchars(trim($quizTitle));
@@ -18,14 +18,19 @@ if (isset($_SESSION["LoggedIN"]) && $_SESSION["LoggedIN"]) {
     $description = htmlspecialchars(trim($description));
     $description = preg_replace("/(?<!\\\\)'/", "it", $description);
     $coverImagePath = "media/";
+    $query3 = "SELECT * FROM quiz WHERE quiz_id = $quiz_id";
+    $result3 = mysqli_query($con, $query3);
+    $quiz = mysqli_fetch_assoc($result3);
+    $img;
     if (isset($_FILES['quiz_cover']['name']) && $_FILES['quiz_cover']['name'] != "") {
         $coverImageExtension = pathinfo($_FILES['quiz_cover']['name'], PATHINFO_EXTENSION);
         $coverImageName = time() . '_' . uniqid() . '.' . $coverImageExtension;
         $coverImageTmpName = $_FILES['quiz_cover']['tmp_name'];
         move_uploaded_file($coverImageTmpName, $coverImagePath . $coverImageName);
+        $img=$coverImagePath.$coverImageName;
     } else {
         // If no new image is provided, keep the existing one
-        $coverImageName = isset($_POST['existing_cover']) ? $_POST['existing_cover'] : 'default.png';
+        $img= $quiz['cover'];
     }
 
     // Check if essential values are not empty before executing the query
@@ -34,11 +39,11 @@ if (isset($_SESSION["LoggedIN"]) && $_SESSION["LoggedIN"]) {
     echo $description;
     if (!empty($quiz_id) && !empty($quizTitle) && !empty($description) ) {
         // Prepare and execute the SQL query
-        $sql = "UPDATE quiz SET title = '$quizTitle', description = '$description', cover = '$coverImagePath$coverImageName' WHERE quiz_id = $quiz_id AND user_id = $user_id;";
-            echo $sql;
+        $sql = "UPDATE quiz SET title = '$quizTitle', description = '$description', cover = '$img' WHERE quiz_id = $quiz_id AND user_id = $user_id;";
+         
         if ($con->query($sql) === TRUE) {
             echo 'success';
-            header("index.php");
+            // header("Location: profile.php");
             
         } else {
             echo "Error: " . $sql . "<br>" . $con->error;
